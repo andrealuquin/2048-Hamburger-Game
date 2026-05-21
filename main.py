0be56afe919e8b63682a131ca97b5016510bac5f
andrealuquin <andrealuquin15@gmail.com>
2026-05-20 18:18:40 -0700

    generated random tiles

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ RECT_WIDTH = HEIGHT // COLS
 
 OUTLINE_COLOR = (201, 66, 56)
 OUTLINE_THICKNESS = 10
-BACKGROUND_COLOR = (205, 192, 180)
+BACKGROUND_COLOR = (252, 165, 157)
 FONT_COLOR = (119, 110, 101)
 
 FONT = pygame.font.SysFont("comicsans", 60, bold=True)
@@ -29,6 +29,51 @@ MOVE_VEL = 20
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048 Hamburger Game")
 
+class Tile:
+    COLORS = [
+        (242, 175, 170),
+        (242, 139, 131),
+        (237, 92, 81),
+        (237, 54, 40),
+        (235, 36, 21),
+        (143, 14, 4),
+        (107, 10, 2),
+        (92, 20, 14),
+        (94, 32, 26),
+    ]
+        
+    def __init__ (self, value, row, col):
+        self.value = value
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
+
+    def get_color(self):
+        color_index = int(math.log2(self.value)) - 1
+        color = self.COLORS[color_index]
+        return color
+
+    def draw(self, window):
+        color = self.get_color()
+        pygame.draw.rect(window,color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
+
+        text = FONT.render(str(self.value), 1, FONT_COLOR)
+        window.blit(
+            text,
+            (
+            (self.x + (RECT_WIDTH / 2 - text.get_width() / 2)),
+            (self.y + (RECT_HEIGHT / 2 - text.get_height() / 2)),
+            ),  
+        )
+ 
+    def set_pos(self):
+        pass
+
+    def move(self, delta):
+        pass
+
+   
 def draw_grid(window):
     for row in range(1, ROWS):
         y = row * RECT_HEIGHT
@@ -39,24 +84,49 @@ def draw_grid(window):
 
     pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
 
-def draw (window):
+def get_random_pos(tiles):
+    row = None
+    col = None
+    while True:
+        row = random.randrange(0, ROWS)
+        col = random.randrange(0, COLS)
+
+        if f"{row}{col}" not in tiles:
+             break
+    return row, col
+ 
+def generate_tiles():
+    tiles = {}
+    for _ in range(2):
+        row, col = get_random_pos(tiles)
+        tiles[f"{row}{col}"] = Tile(2, row, col)
+    return tiles
+
+def draw (window, tiles):
     window.fill(BACKGROUND_COLOR)
-    draw_grid(window)
+
+    for tile in tiles.values():
+        tile.draw(window)
+    draw_grid(window) 
     pygame.display.update()
 
 def main(window):
     clock = pygame.time.Clock()
     run = True
+
+    tiles = generate_tiles()
+
+  
     while run:
-        clock.tick(FPS)
+        clock.tick(FPS) 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-        draw(window)
+        draw(window, tiles)
     pygame.quit()
 
 if __name__ == "__main__":
-    main(WINDOW)
+    main(WINDOW) 
 
 
