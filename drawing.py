46371c995dfd144fbdae2cc8798ca1e49aaf3eea
andrealuquin <andrealuquin15@gmail.com>
2026-05-25 15:53:14 -0700

    separated drawing functions

---
+++ drawing.py
@@ -0,0 +1,33 @@
+import pygame
+
+from settings import (
+    ROWS,
+    COLS,
+    WIDTH,
+    HEIGHT,
+    RECT_WIDTH,
+    RECT_HEIGHT,
+    OUTLINE_COLOR,
+    OUTLINE_THICKNESS,
+    BACKGROUND_COLOR,
+)
+
+def draw_grid(window):
+    for row in range(1, ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH,y), OUTLINE_THICKNESS)
+    for col in range(1, COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+def draw (window, tiles):
+    window.fill(BACKGROUND_COLOR)
+
+    for tile in tiles.values():
+        tile.draw(window)
+
+    draw_grid(window) 
+
+    pygame.display.update()
