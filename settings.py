d67056d4b0030d9db772243cd2026a26d0748158
andrealuquin <andrealuquin15@gmail.com>
2026-05-25 15:45:13 -0700

    added settings file for grid config

---
+++ settings.py
@@ -0,0 +1,26 @@
+import pygame
+import random
+import math
+
+pygame.init()
+
+# frames per second
+FPS = 60
+
+# grid display
+WIDTH, HEIGHT = 800, 800
+ROWS = 4
+COLS = 4
+
+#tile height
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
+
+OUTLINE_COLOR = (201, 66, 56)
+OUTLINE_THICKNESS = 10
+BACKGROUND_COLOR = (252, 165, 157)
+FONT_COLOR = (119, 110, 101)
+
+FONT = pygame.font.SysFont("comicsans", 60, bold=True)
+#speed of tiles
+MOVE_VEL = 20
