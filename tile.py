41f7f88e7a1a49d2f63b75aac59ec693cf900de9
andrealuquin <andrealuquin15@gmail.com>
2026-07-11 17:29:52 -0700

    imported plate use in game

--- tile.py
+++ tile.py
@@ -1,7 +1,11 @@
 import pygame
 import random
+import os
 import math
 
+TILE_IMAGES = {
+    2: pygame.image.load("assets/images/Plate.png")
+}
 # for each tile: value, row and column position on board
 # and pixel position for dreawing 
 from settings import RECT_WIDTH, RECT_HEIGHT, FONT, FONT_COLOR
@@ -35,16 +39,22 @@ class Tile:
     def draw(self, window):
         color = self.get_color()
         pygame.draw.rect(window,color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
+              # if this tile value has an image, draw the image on top.
+        if self.value in TILE_IMAGES:
+            image = TILE_IMAGES[self.value]
+            image = pygame.transform.scale(image, (RECT_WIDTH, RECT_HEIGHT))
+            window.blit(image, (self.x, self.y))
 
-        text = FONT.render(str(self.value), 1, FONT_COLOR)
-
-        window.blit(
-            text,
-            (
-            (self.x + (RECT_WIDTH / 2 - text.get_width() / 2)),
-            (self.y + (RECT_HEIGHT / 2 - text.get_height() / 2)),
-            ),  
-        )
+        # if there is no image yet, draw the number.
+        else:
+            text = FONT.render(str(self.value), 1, FONT_COLOR)
+            window.blit(
+                text,
+                (
+                    self.x + (RECT_WIDTH / 2 - text.get_width() / 2),
+                    self.y + (RECT_HEIGHT / 2 - text.get_height() / 2),
+                ),
+            )
  
     def set_pos(self, ceil= False):
         if ceil:
